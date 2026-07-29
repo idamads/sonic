3131a77202200dbeb41fda3509fe2101554263c1
idamads <idamads@ukr.net>
2026-07-29 17:59:03 +0300

    Create menu.py

---
+++ src/cli/menu.py
@@ -0,0 +1,10 @@
+def show_menu():
+
+    print()
+
+    print("1. Add recipe")
+    print("2. Search")
+    print("3. Favorites")
+    print("4. Statistics")
+    print("5. Export")
+    print("6. Exit")
